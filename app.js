40adba7e1dd0cd0d749ee33d10722d54f33ddb9a
Taha <Taha.Hassanpour@outlook.com>
2022-04-08 03:29:14 +0430

    validate form

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ let submitError = document.getElementById("submit-error");
 function validateName(){
     let name = document.getElementById('contact-name').value;
     if(name.length === 0){
-        nameError.innerHTML = 'Name is required';
+         nameError.style.display = "block";
+         nameError.innerHTML = 'Name is required';
+         setTimeout(function () {
+          nameError.style.display = "none";
+         }, 3000);
         return false;
     }
 
@@ -27,7 +31,11 @@ function validatePhone(){
     let phone = document.getElementById('contact-phone').value;
 
     if(phone.length === 0){
-        phoneError.innerHTML = 'phone is required';
+         phoneError.style.display = "block";
+         phoneError.innerHTML = 'phone is required';
+         setTimeout(function () {
+           phoneError.style.display = "none";
+         }, 3000);
         return false;
     }
     if(phone.length !== 10){
@@ -49,7 +57,11 @@ function validateEmail(){
 
 
     if(email.length === 0){
-        emailError.innerHTML = 'Email is required';
+         emailError.style.display = "block";
+         emailError.innerHTML = 'Email is required';
+         setTimeout(function () {
+           emailError.style.display = "none";
+         }, 3000);
         return false;
     };
 
@@ -78,6 +90,15 @@ function validateMessage(){
 
      messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
        return true;
+}
+
+function validateForm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){  
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = "none";},3000)
+        return false; 
+    }
 }
 
 
